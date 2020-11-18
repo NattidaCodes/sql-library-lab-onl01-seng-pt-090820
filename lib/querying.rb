35c49effce8648cae76fa0801b3a19808731af79
Nattida Wong <nattidac.wong@gmail.com>
2020-11-18 10:30:51 -0500

    commit tree 6957f41a37691ee917e89a0f0b392418f14b3106

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,61 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "
+  SELECT books.title, books.year
+  FROM books
+  WHERE series_id = 1
+  ORDER BY(year);
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "
+  SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC LIMIT(1);
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY(species)
+  ORDER BY COUNT(species) DESC LIMIT(1);
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  SELECT authors.name, subgenres.name
+  FROM series
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id
+  JOIN authors
+  ON authors.id = series.author_id;
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  SELECT series.title
+  FROM series
+  LEFT OUTER JOIN characters
+  ON characters.series_id = series.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species = 'human') DESC 
+  LIMIT 1;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  SELECT characters.name, character_books.character_id
+  FROM characters
+  JOIN character_books
+  ON character_books.id = characters.character_books
+  GROUP BY(characters.name) 
+  ORDER BY(characters.name) DESC;
+  "
 end
